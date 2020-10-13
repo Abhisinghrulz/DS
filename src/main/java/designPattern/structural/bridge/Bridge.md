29bb470922bf485267f67cd9bb431cf2033f9b6b
Abhishek <abhisinghrulz@gmail.com>
2020-10-13 16:37:53 +0530

    Bridge Design Pattern

---
+++ src/main/java/designPattern/structural/bridge/Bridge.md
@@ -0,0 +1,3 @@
+Decouple an abstraction from its implementation so that the two can vary independently
+
+https://www.journaldev.com/1491/bridge-design-pattern-java
---
+++ src/main/java/designPattern/structural/bridge/RedColor.java
@@ -0,0 +1,8 @@
+package designPattern.structural.bridge;
+
+public class RedColor implements Color{
+
+    public void applyColor(){
+        System.out.println("red.");
+    }
+}
